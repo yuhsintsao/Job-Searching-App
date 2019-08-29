a27c967e34c945726da7d9890d471adf3a45ba14
Yu-Hsin Tsao <yuhsin.tsao@gmail.com>
2019-08-30 01:12:33 +0800

    import csv data into mongodb

--- app.py
+++ app.py
@@ -2,22 +2,36 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 from views.menu import menu_blueprint
 from models.auth import Auth, ErrorHandlers, required_login
 from common.database import Database
+from pymongo import MongoClient
 import os
+import pandas as pd
 
 app = Flask(__name__) # '__main__'
 app.secret_key = "0000"
 app.config.update(
     DB_URI=os.environ.get('MONGODB_URI'),
     ADMIN=os.environ.get('ADMIN'),
-    DB_NAME='jobopenings',
+    DB_NAME=os.environ.get('DEFAULT_DB'),
 )
+
+def insertStaticDataIntoDatabase():
+    client = pymongo.MongoClient(app.config['DB_URI'])
+    company = client[app.config['DB_NAME']]['company']
+    job = client[app.config['DB_NAME']]['job']
+
+    dict_job = pd.read_csv("./data/NVDA.csv").to_dict(orient='records')
+    dict_company = pd.read_csv("./data/iNVDA.csv").to_dict(orient='records')
+
+    company.insert_many(dict_company)
+    job.insert_many(dict_job)
+    client.close()
 
 app.register_blueprint(menu_blueprint, url_prefix="/menu")
 
 @app.before_first_request   
 def initialize_database():
-    #Database.initialize(app.config['DB_URI'], app.config['DB_NAME'])
-    pass
+    insertStaticDataIntoDatabase()
+    Database.initialize(app.config['DB_URI'], app.config['DB_NAME'])
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
